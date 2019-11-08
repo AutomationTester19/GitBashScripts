fa5c56d0da2e53e863d8e4613db5ef0ac7767e68
Your Name <you@example.com>
2019-11-08 21:55:11 +0530

    updated branch

--- commit.sh
+++ commit.sh
@@ -3,9 +3,9 @@
 
 
 commit_messgae = "$1"
-branch_name = "$branchname"
+branch_name = $(git branch | grep "*")
 git add .
 git commit -m "$1"
-git push -u origin "$branchname"
+git push -u origin "$branch_name"
 
 
