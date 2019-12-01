33d594aa37a6bbdbbfade39f1ff6e53ab47bc822
Your Name <you@example.com>
2019-12-01 22:12:40 +0530

    "new dev"

--- commit.sh
+++ commit.sh
@@ -5,13 +5,14 @@ read -p " commit_messgae       : " desc
 
 git add .
 git commit -m "$desc"
-git push -u origin "$branchname"
 if [ -n "$branchname" ];
 then
 echo "User Cannot Commit to $branchname as it is empty"
 else 
 read -p " Enter branch name    : " branchname
 echo "User Entered Branch Name And Branch Name is " $branchname
+git push -u origin "$branchname"
+
 fi
 echo "Commited File SuccessFully, Check the GitHub Repo"
 
