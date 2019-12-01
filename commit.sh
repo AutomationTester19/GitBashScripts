500d892a5a58f0057ee7fe4c1e5428df46ffae1c
Your Name <you@example.com>
2019-12-01 21:02:11 +0530

    updated exact branch

--- commit.sh
+++ commit.sh
@@ -5,5 +5,5 @@ read -p " Enter branch name   : " branchname
 read -p "commit_messgae:  " desc
 git add .
 git commit -m "$desc"
-git push -u origin "$branch"
+git push -u origin "$branchname"
 echo $"Test Completed"
