dd62f2809820f66d22a1f459736f1c330c43d514
Your Name <you@example.com>
2019-12-01 21:00:44 +0530

    updated branch code

--- commit.sh
+++ commit.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 
-
+read -p " Enter branch name   : " branchname
 read -p "commit_messgae:  " desc
 git add .
 git commit -m "$desc"
+git push -u origin "$branch"
 echo $"Test Completed"
