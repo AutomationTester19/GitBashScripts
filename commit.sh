e683fd74304763f99b12ca976f4a9b895b544fe8
Your Name <you@example.com>
2019-12-01 22:28:37 +0530

    check branch

--- commit.sh
+++ commit.sh
@@ -7,16 +7,17 @@ git add .
 git commit -m "$desc"
 
 read -p "Enter branch name    : "    branchname
+
 if [ -z "$branchname" ];
 then
 echo "User Cannot Commit to $branchname as it is empty"
-else 
+else
+str = git $branchname 
 echo "User Entered Branch Name And Branch Name is " $branchname
 fi
 
 git push -u origin "$branchname"
-
-if [ -z "$branchname"]
+if [ -z "$branchname" && str==$branchname]
 then
         echo "User Cannot Commit the file to empty branch"
 else 
